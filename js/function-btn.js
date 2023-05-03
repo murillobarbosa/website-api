e665394a433d32d4f738b964c8f21ec7414be912
MURILLO RIBEIRO BARBOSA <22282112@CFP127ALUNO.LOCAL>
2023-05-03 11:19:13 -0300

    projeto em andamento

--- js/function-btn.js
+++ js/function-btn.js
@@ -1,5 +1,7 @@
 const myButton = document.getElementById('myButton');
 
 myButton.addEventListener('click', () => {
-    window.location.href = 'http://127.0.0.1:5501/pages/movie.html';
-  });
+    window.location.href = 'http://127.0.0.1:5500/pages/movie.html';
+});
+
+// console.log(myButton);
