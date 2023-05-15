fb5dd138584ae332717e246341b95fd4856f38b4
MURILLO RIBEIRO BARBOSA <22282112@CFP127ALUNO.LOCAL>
2023-05-15 07:54:47 -0300

    entrega

--- js/function-btn.js
+++ js/function-btn.js
@@ -1,6 +1,7 @@
 const myButton = document.getElementById('myButton');
 myButton.addEventListener('click', () => {
-    window.location.href = 'http://127.0.0.1:5500/pages/movie.html';
+    // window.location.href = 'http://127.0.0.1:5500/pages/movie.html';
+    window.open('../pages/movie.html')
 });
 
 
